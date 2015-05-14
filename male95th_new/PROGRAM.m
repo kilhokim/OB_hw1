57cd3d78be305a7ab9d5e947def4af8c003c0982
catharsist <catharsistk@gmail.com>
2015-05-14 16:29:58 +0900

    Add a new function THICKNESS to make it return thickness info for body parts

--- male95th_new/PROGRAM.m
+++ male95th_new/PROGRAM.m
@@ -1,57 +1,63 @@
+%% *************************************************************
+% Occupational Biomechanics
+% Homework #1
+% Industrial Engineering, Seoul National University
+%
+% author: Kilho Kim, Hakeun Jin
+% **************************************************************
 close all;
 clear all;
 
-% Human Linkage
 
+%% Human Linkage
+% IMPORTANT: This must be predefined 
+% before generating postures of the fixed stature
 stature = 185;
 L = LINK(stature);
 
-% ADDED: The thickness of the body:
-r1 = 5.53;
-r2 = 8.69;
-r3 = 13.36;
-r4 = 3.59;
-r5 = 2.37;
-R = [r1 r2 r3 r4 r5];
 
-% input variables
-
+%% Input variables
+gender = input('Gender(Man=1,WOMAN=0) = ');
+age = input('Age = ');
 X_tar = input('X_tar = ');
 Y_tar = input('Y_tar = ');
 load_tar = input('Load mass = ');
 flag_volume = input('Add volume to the body(TRUE=1, FALSE=0) = ');
-%am = input('Asymmetry Multiplier = ');
-%cm = input('Coupling Multiplier = ');
-%fm = input('Frequency Multiplier = ');
+
+% R contains the thickness info of 5 body parts
+R = THICKNESS(gender, age);
 
 % The coordinates of the target (width=10, height=10)
 T = [X_tar Y_tar];
 
+
+%% Obstacles Configuration
+% NOTE: This should be predefined before running
+
 % Obstacle Configuration: Box
 % B = [x y w h; x y w h; ...]
-
-%B = [20 140 60 20; 20 60 60 60; 60 120 20 60; 20 90 10 30];
-
-%B = [-60 150 220 20];
-
-%B = [-40 120 30 120; 40 120 30 120];
-%B = [ 10 135 10 135; -20 130 10 130];
-%B = [];
-
-%B = [25 105 20 105];
-
-% Coordinates of the obstacle is automatically
-% defined, referring to the target locations
-B = [X_tar 0 10 Y_tar;
-     X_tar Y_tar+10 10 Y_tar]; % [X, Y, width, height]
-
 % Obstacle Configuration: Sphere
 % C = [x y r; x y r; ...]
 
-C = [30 80 10];
+% Posture #1(Semiconductor factory)
+% The number of possible postures decreases
+% due to the volume of the knee part if x=30, y=80
+% B = [X_tar-10 0 30 Y_tar; X_tar-10 Y_tar+20 30 50; X_tar+20 0 10 150]; % [X, Y, width, height]
 
+% Posture #2(Stairs)
+% Knee part hits the obstacle if x=50, y=50
+%B = [X_tar-20 0 20 Y_tar-10;
+%     X_tar 0 20 Y_tar; X_tar+20 0 20 Y_tar+20]; % [X, Y, width, height]
+
+% posture #3(automobile)
+% x=30, y= 160, 170, 180
+height = Y_tar+10;
+B = [-30 height 150 20;]; % [X, Y, width, height]
+C = [-30+30 height-10 10; -30+150-30 height-10 10];
+
+
+%% Load postures data from outer CELLs
 % Loading the relevant cell
-
 XX = num2str(floor(X_tar/10)*10);
 YY = num2str(floor(Y_tar/10)*10);
 
@@ -64,8 +70,9 @@ postures = load(fullfile('CELL', cell_name));
 temp = size(postures);
 numP = temp(1);
 
-% Draw World
 
+%% Draw world, target and obstacles
+% Draw World
 figure (1);
 axis([-60 140 0 240]);
 
@@ -82,24 +89,21 @@ TPLOT(T);
 BPLOT(B,1);
 CPLOT(C,1);
 
-%fill([X_tar X_tar 73 73 X_tar],[Y_tar 185 185 Y_tar Y_tar], 'r');
-
 % Fill the target with blue
 fill([X_tar X_tar+10 X_tar+10 X_tar X_tar], [Y_tar Y_tar Y_tar+10 Y_tar+10 Y_tar],'b');
 
 
-% draw all postures in the loaded cell
-
-% Change the value below to set the number of postures
+%% Draw all postures in the loaded cells
+% NOTE: Change the value below to set the number of postures
+% To set # to the maximum, change the value to numP
 numPlot = 3;
 REBAs = zeros(numPlot, 1);
 
 % Plot for every posture in postures array
 for i=1:1:numPlot
    posture = postures(i,:);
-   % Checking collision 
-   % if there's collision, potential > 0
-   potential = POTENTIAL2(L, posture, B, C, flag_volume, R)
+   % Checking collision: if there's collision, potential > 0
+   potential = POTENTIAL2(L, posture, B, C, flag_volume, R);
    
    % Visualize plot only if there is no collision
    if (potential == 0)
@@ -108,34 +112,3 @@ for i=1:1:numPlot
    end;
    
 end;
-
-% FIXME: Why 63 and 175??
-%for x = X_tar:63,
-%    for y = Y_tar:175,
-        % Calculate the Recommended Weight Limit(RWL) 
-        % RWL = LC x HM x VM x DM x AM x FM x CM
-        % HM, the Horizontal Multiplier factor
-        % VM, the Vertical Multiplier factor
-        % DM, the Distance Multiplier factor
-        % FM, the Frequency Multiplier factor
-        % AM, the Asymmetric Multiplier factor
-        % CM, the Coupling Multiplier factor
-        % RWL, the Recommended Weight Limit
-        %RWL = 23 * (25/x) * (1-(0.003 * abs(y-75))) * (0.82 + (4.5/(y-Y_tar))) * am * cm * fm;
-        % If RWL is over one third of load target weight,
-        % Plot the imaginary target
-        %if (RWL > (1/3 * load_tar))
-            % Fill the imaginary target with green
-            %fill([x x x+10 x+10 x], [y y+10 y+10 y y], 'g');
-        %end;
-%    end;
-%end;
-
-
-
-
-
-
-
-
-
---
+++ male95th_new/THICKNESS.m
@@ -0,0 +1,90 @@
+function R = THICKNESS(gender, age)
+% Calculate the thickness of the body, with regard to input gender and age
+
+% The thickness of the body:
+% r1: lower leg, r2: upper leg, r3: breast, 
+% r4: upper arm, r5: lower arm
+
+if(gender == 1 && 0 < age <30)
+r1 = 5.97;
+r2 = 9.02;
+r3 = 15.27;
+r4 = 4.12;
+r5 = 2.67;
+end
+
+if(gender == 1 && 30 <= age < 40)
+r1 = 6.01;
+r2 = 9.15;
+r3 = 15.78;
+r4 = 4.14;
+r5 = 2.67;
+end
+
+if(gender == 1 && 40 <= age < 50)
+r1 = 6.06;
+r2 = 9.01;
+r3 = 15.74;
+r4 = 3.59;
+r5 = 2.71;
+end
+
+if(gender == 1 && 50 <= age < 60)
+r1 = 5.81;
+r2 = 8.40;
+r3 = 15.34;
+r4 = 3.96;
+r5 = 2.69;
+end
+
+if(gender == 1 && 60 <= age)
+r1 = 5.78;
+r2 = 8.40;
+r3 = 15.33;
+r4 = 3.96;
+r5 = 2.72;
+end
+
+if(gender == 0 && 0 < age <30)
+r1 = 5.53;
+r2 = 8.69;
+r3 = 13.36;
+r4 = 3.59;
+r5 = 2.37;
+end
+
+if(gender == 0 && 30 <= age <40)
+r1 = 5.54;
+r2 = 8.61;
+r3 = 13.74;
+r4 = 3.66;
+r5 = 2.41;
+end
+
+if(gender == 0 && 40 <= age <50)
+r1 = 5.57;
+r2 = 8.63;
+r3 = 14.15;
+r4 = 3.66;
+r5 = 2.44;
+end
+
+if(gender == 0 && 50 <= age <60)
+r1 = 5.54;
+r2 = 8.82;
+r3 = 14.44;
+r4 = 3.80;
+r5 = 2.55;
+end
+
+if(gender == 0 && 60 <= age)
+r1 = 5.47;
+r2 = 8.58;
+r3 = 14.56;
+r4 = 3.82;
+r5 = 2.58;
+end
+
+R = [r1 r2 r3 r4 r5];
+
+end
