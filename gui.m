242f9729258e70b425b639eb39a8d5d1dd9c3cd9
catharsist <catharsistk@gmail.com>
2015-06-06 17:11:11 +0900

    Add features for editing personal information and spotting coordinates on plot

--- gui.m
+++ gui.m
@@ -22,7 +22,7 @@ function varargout = gui(varargin)
 
 % Edit the above text to modify the response to help gui
 
-% Last Modified by GUIDE v2.5 06-Jun-2015 13:07:37
+% Last Modified by GUIDE v2.5 06-Jun-2015 17:02:50
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -53,14 +53,36 @@ function gui_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   command line arguments to gui (see VARARGIN)
 
 % Create the data to plot
-handles.peaks = peaks(35);
-handles.membrane = membrane;
-[x, y] = meshgrid(-8:0.5:8);
-r = sqrt(x.^2 + y.^2) + eps;
-sinc = sin(r)./r;
-handles.sinc = sinc;
-handles.current_data = handles.peaks;
-surf(handles.current_data);
+% handles.hPlotAxes = axes('Parent', hObject, ...
+%                  'XLim', [-60 140], ...
+%                  'YLim', [0 200], ...
+%                  'XTick', -60:10:140, ...
+%                  'YTick', 0:10:200, ...
+%                  'Position', [0.08 0.1 0.6 0.8]);
+                    
+% handles.hPlotAxes.Title.String = 'Total possible postures';
+title('Total possible postures');
+axis([-60 140 0 200]);           
+%set(handles.world_axes, 'ButtonDownFcn', @AxesClickCallback);
+
+set(gca, 'Xtick', -60:10:140);
+set(gca, 'Ytick', 0:10:200);
+grid on;
+hold on;
+
+handles.stature = 170;
+handles.gender = 1;
+handles.age = 25;
+handles.load_tar = 5;
+
+% handles.peaks = peaks(35);
+% handles.membrane = membrane;
+% [x, y] = meshgrid(-8:0.5:8);
+% r = sqrt(x.^2 + y.^2) + eps;
+% sinc = sin(r)./r;
+% handles.sinc = sinc;
+% handles.current_data = handles.peaks;
+% surf(handles.current_data);
 
 % Choose default command line output for gui
 handles.output = hObject;
@@ -70,6 +92,15 @@ guidata(hObject, handles);
 
 % UIWAIT makes gui wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
+
+
+function AxesClickCallback ( objectHandle , eventData )
+axesHandle  = get(objectHandle,'Parent');
+%coordinates = get(axesHandle,'CurrentPoint'); 
+%coordinates = coordinates(1,1:2);
+coordinates = ginput(1);
+message     = sprintf('x: %.1f , y: %.1f',coordinates (1) ,coordinates (2));
+helpdlg(message);
 
 
 % --- Outputs from this function are returned to the command line.
@@ -89,18 +120,7 @@ function draw_obs_button_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-updateAxes(handles)
-
-
-
-function stature_edit_Callback(hObject, eventdata, handles)
-% hObject    handle to stature_edit (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of stature_edit as text
-%        str2double(get(hObject,'String')) returns contents of stature_edit as a double
-
+AxesClickCallback(hObject, eventdata)
 
 
 % --- Executes during object creation, after setting all properties.
@@ -115,39 +135,13 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-
-function updateAxes(handles)
-var = get(handles.stature_edit, 'string');
-var = str2double(var);
-surf(membrane(var))
-
-
-% --- Executes on button press in gender_male.
-function gender_male_Callback(hObject, eventdata, handles)
-% hObject    handle to gender_male (see GCBO)
+function stature_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hint: get(hObject,'Value') returns toggle state of gender_male
-
-
-% --- Executes on button press in gender_female.
-function gender_female_Callback(hObject, eventdata, handles)
-% hObject    handle to gender_female (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of gender_female
-
-
-
-function age_edit_Callback(hObject, eventdata, handles)
-% hObject    handle to age_edit (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of age_edit as text
-%        str2double(get(hObject,'String')) returns contents of age_edit as a double
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
 
 
 % --- Executes during object creation, after setting all properties.
@@ -162,32 +156,84 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+function age_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
+
 
 % --- Executes on button press in confirm_button.
 function confirm_button_Callback(hObject, eventdata, handles)
 % hObject    handle to confirm_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles.stature = str2double(get(handles.stature_edit, 'string'));
+curr_gender = get(handles.gender_popupmenu, 'Value');
+if curr_gender == 2
+    handles.gender = 0;
+end
 
-
-% --- Executes on selection change in listbox2.
-function listbox2_Callback(hObject, eventdata, handles)
-% hObject    handle to listbox2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns listbox2 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from listbox2
+handles.age = str2double(get(handles.age_edit, 'string'));
+handles.load_tar = str2double(get(handles.target_weight_edit, 'string'));
+% fprintf('handles.stature=%d', handles.stature);
+% fprintf('handles.gender=%d', handles.gender);
+% fprintf('handles.age=%d', handles.age);
+% fprintf('handles.load_tar=%d', handles.load_tar);
+message = sprintf('stature: %d , gender: %d, age: %d, load_tar: %d', ...
+        handles.stature, handles.gender, handles.age, handles.load_tar);
+helpdlg(message);
 
 
 % --- Executes during object creation, after setting all properties.
-function listbox2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to listbox2 (see GCBO)
+function gender_popupmenu_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to gender_popupmenu (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: listbox controls usually have a white background on Windows.
+% Hint: popupmenu controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+function gender_popupmenu_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
+
+
+% --- Executes on button press in draw_target_button.
+function draw_target_button_Callback(hObject, eventdata, handles)
+% hObject    handle to draw_target_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function target_weight_edit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to target_weight_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function target_weight_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
